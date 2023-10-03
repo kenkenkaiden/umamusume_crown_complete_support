161c22c9697a73a51af72a03f44d12759ebabb01
kenkenkaiden <allexhard2000@gmail.com>
2023-10-03 22:26:37 +0900

    ウマ娘登録・解除ページ立ち上げ

--- app/controllers/umamusumes_controller.rb
+++ app/controllers/umamusumes_controller.rb
@@ -2,4 +2,8 @@ class UmamusumesController < ApplicationController
   def index
     @umamusumes = Umamusume.all
   end
+
+  def new
+    @umamusume = Umamusume.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'umamusumes#index'
-  resources :umamusumes, only:[:index]
+  resources :umamusumes, only:[:index, :new, :create]
 end
