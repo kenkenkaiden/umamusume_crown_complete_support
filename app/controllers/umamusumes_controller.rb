7a6d5c65794ca5795cfafd01f5e4540f2973fa2d
kenkenkaiden <allexhard2000@gmail.com>
2023-11-01 21:39:59 +0900

    著作権に触れないトロフィーの導入と触れるトロフィーの削除

--- app/controllers/umamusumes_controller.rb
+++ app/controllers/umamusumes_controller.rb
@@ -2,16 +2,12 @@ class UmamusumesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    if user_signed_in?
-      @umamusumes = current_user.umamusumes
-    end
+    @umamusumes = current_user.umamusumes if user_signed_in?
   end
-
 
   def new
     set_umamusume_list
   end
-
 
   def create
     @umamusume = Umamusume.new(umamusume_params)
@@ -29,7 +25,6 @@ class UmamusumesController < ApplicationController
     @races = Race.order("default_order ASC")
   end
 
-
   def destroy
     @umamusume = Umamusume.find(params[:id])
     if @umamusume.destroy
@@ -38,81 +33,13 @@ class UmamusumesController < ApplicationController
     end
   end
 
-
   private
-
 
   def umamusume_params
     params.require(:umamusume).permit(:name, :turf, :dirt, :sprint, :mile, :intermediate, :long).merge(user_id: current_user.id)
   end
 
-
-  def set_umamusume_list
-    @umamusume_list = [
-      { name: "スペシャルウィーク", turf: "A", dirt: "G", sprint: "F", mile: "C", intermediate: "A", long: "A" },
-      { name: "サイレンススズカ", turf: "A", dirt: "G", sprint: "D", mile: "A", intermediate: "A", long: "E" },
-      { name: "トウカイテイオー", turf: "A", dirt: "G", sprint: "F", mile: "E", intermediate: "A", long: "B" },
-      { name: "マルゼンスキー", turf: "A", dirt: "D", sprint: "B", mile: "A", intermediate: "B", long: "C" },
-      { name: "フジキセキ", turf: "A", dirt: "F", sprint: "B", mile: "A", intermediate: "B", long: "E" },
-      { name: "オグリキャップ", turf: "A", dirt: "B", sprint: "E", mile: "A", intermediate: "A", long: "B" },
-      { name: "ゴールドシップ", turf: "A", dirt: "G", sprint: "G", mile: "C", intermediate: "A", long: "A" },
-      { name: "ウオッカ", turf: "A", dirt: "G", sprint: "F", mile: "A", intermediate: "A", long: "F" },
-      { name: "ダイワスカーレット", turf: "A", dirt: "G", sprint: "F", mile: "A", intermediate: "A", long: "B" },
-      { name: "タイキシャトル", turf: "A", dirt: "B", sprint: "A", mile: "A", intermediate: "E", long: "G" },
-      { name: "グラスワンダー", turf: "A", dirt: "G", sprint: "G", mile: "A", intermediate: "B", long: "A" },
-      { name: "ヒシアマゾン", turf: "A", dirt: "E", sprint: "D", mile: "A", intermediate: "A", long: "B" },
-      { name: "メジロマックイーン", turf: "A", dirt: "E", sprint: "G", mile: "F", intermediate: "A", long: "A" },
-      { name: "エルコンドルパサー", turf: "A", dirt: "B", sprint: "F", mile: "A", intermediate: "A", long: "B" },
-      { name: "テイエムオペラオー", turf: "A", dirt: "E", sprint: "G", mile: "E", intermediate: "A", long: "A" },
-      { name: "ナリタブライアン", turf: "A", dirt: "G", sprint: "F", mile: "B", intermediate: "A", long: "A" },
-      { name: "シンボリルドルフ", turf: "A", dirt: "G", sprint: "E", mile: "C", intermediate: "A", long: "A" },
-      { name: "エアグルーヴ", turf: "A", dirt: "G", sprint: "C", mile: "B", intermediate: "A", long: "E" },
-      { name: "アグネスデジタル", turf: "A", dirt: "A", sprint: "F", mile: "A", intermediate: "A", long: "G" },
-      { name: "セイウンスカイ", turf: "A", dirt: "G", sprint: "G", mile: "C", intermediate: "A", long: "A" },
-      { name: "タマモクロス", turf: "A", dirt: "F", sprint: "G", mile: "E", intermediate: "A", long: "A" },
-      { name: "ファインモーション", turf: "A", dirt: "G", sprint: "F", mile: "A", intermediate: "A", long: "C" },
-      { name: "ビワハヤヒデ", turf: "A", dirt: "F", sprint: "F", mile: "C", intermediate: "A", long: "A" },
-      { name: "マヤノトップガン", turf: "A", dirt: "E", sprint: "D", mile: "D", intermediate: "A", long: "A" },
-      { name: "マンハッタンカフェ", turf: "A", dirt: "G", sprint: "G", mile: "F", intermediate: "B", long: "A" },
-      { name: "ミホノブルボン", turf: "A", dirt: "G", sprint: "C", mile: "B", intermediate: "A", long: "B" },
-      { name: "メジロライアン", turf: "A", dirt: "G", sprint: "E", mile: "C", intermediate: "A", long: "B" },
-      { name: "ヒシアケボノ", turf: "A", dirt: "F", sprint: "A", mile: "B", intermediate: "F", long: "G" },
-      { name: "ユキノビジン", turf: "A", dirt: "B", sprint: "D", mile: "A", intermediate: "A", long: "E" },
-      { name: "ライスシャワー", turf: "A", dirt: "G", sprint: "E", mile: "C", intermediate: "A", long: "A" },
-      { name: "アイネスフウジン", turf: "A", dirt: "G", sprint: "G", mile: "A", intermediate: "A", long: "C" },
-      { name: "アグネスタキオン", turf: "A", dirt: "G", sprint: "G", mile: "D", intermediate: "A", long: "B" },
-      { name: "アドマイヤベガ", turf: "A", dirt: "G", sprint: "F", mile: "C", intermediate: "A", long: "C" },
-      { name: "イナリワン", turf: "A", dirt: "A", sprint: "F", mile: "B", intermediate: "A", long: "A" },
-      { name: "ウイニングチケット", turf: "A", dirt: "G", sprint: "G", mile: "F", intermediate: "A", long: "B" },
-      { name: "エアシャカール", turf: "A", dirt: "G", sprint: "G", mile: "E", intermediate: "A", long: "A" },
-      { name: "エイシンフラッシュ", turf: "A", dirt: "G", sprint: "G", mile: "F", intermediate: "A", long: "A" },
-      { name: "カレンチャン", turf: "A", dirt: "F", sprint: "A", mile: "D", intermediate: "G", long: "G" },
-      { name: "カワカミプリンセス", turf: "A", dirt: "G", sprint: "D", mile: "B", intermediate: "A", long: "F" },
-      { name: "ゴールドシチー", turf: "A", dirt: "D", sprint: "F", mile: "A", intermediate: "B", long: "B" },
-      { name: "サクラバクシンオー", turf: "A", dirt: "G", sprint: "A", mile: "B", intermediate: "G", long: "G" },
-      { name: "シーキングザパール", turf: "A", dirt: "F", sprint: "A", mile: "A", intermediate: "E", long: "G" },
-      { name: "シンコウウインディ", turf: "F", dirt: "A", sprint: "C", mile: "A", intermediate: "B", long: "G" },
-      { name: "スイープトウショウ", turf: "A", dirt: "G", sprint: "E", mile: "A", intermediate: "A", long: "D" },
-      { name: "スーパークリーク", turf: "A", dirt: "G", sprint: "G", mile: "G", intermediate: "A", long: "A" },
-      { name: "スマートファルコン", turf: "E", dirt: "A", sprint: "B", mile: "A", intermediate: "A", long: "E" },
-      { name: "ゼンノロブロイ", turf: "A", dirt: "G", sprint: "G", mile: "E", intermediate: "A", long: "A" },
-      { name: "トーセンジョーダン", turf: "A", dirt: "G", sprint: "G", mile: "F", intermediate: "A", long: "B" },
-      { name: "ナカヤマフェスタ", turf: "A", dirt: "G", sprint: "G", mile: "C", intermediate: "A", long: "B" },
-      { name: "ナリタタイシン", turf: "A", dirt: "G", sprint: "G", mile: "D", intermediate: "A", long: "A" },
-      { name: "ニシノフラワー", turf: "A", dirt: "F", sprint: "A", mile: "A", intermediate: "E", long: "G" },
-      { name: "ハルウララ", turf: "G", dirt: "A", sprint: "A", mile: "B", intermediate: "G", long: "G" },
-      { name: "バンブーメモリー", turf: "A", dirt: "D", sprint: "A", mile: "A", intermediate: "C", long: "G" },
-      { name: "マーベラスサンデー", turf: "A", dirt: "F", sprint: "G", mile: "C", intermediate: "A", long: "B" },
-      { name: "マチカネフクキタル", turf: "A", dirt: "F", sprint: "F", mile: "C", intermediate: "A", long: "A" },
-      { name: "ミスターシービー", turf: "A", dirt: "G", sprint: "G", mile: "B", intermediate: "A", long: "A" },
-      { name: "メイショウドトウ", turf: "A", dirt: "E", sprint: "G", mile: "F", intermediate: "A", long: "A" },
-      { name: "メジロドーベル", turf: "A", dirt: "G", sprint: "E", mile: "A", intermediate: "A", long: "F" },
-      { name: "ナイスネイチャ", turf: "A", dirt: "G", sprint: "G", mile: "C", intermediate: "A", long: "A" },
-    ]
-
-    @umamusume_list.each do |umamusume|
-      umamusume[:image] = "#{umamusume[:name]}（公式SNSアイコン）.png"
-    end
+  def set_umamusume_list # ウマ娘たちのデータを用意するためのメソッド
+    @umamusume_list = UmamusumeListService.get_list
   end
-
 end
