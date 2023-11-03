189f7c5052fcd1701fd2116cdc3e8f44a3f039d6
kenkenkaiden <allexhard2000@gmail.com>
2023-11-04 07:24:32 +0900

    登録済みのレースを削除する機能を実装

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,9 +1,31 @@
 class RacesController < ApplicationController
   def create
-    # レースの登録処理をここに実装
+    @umamusume = Umamusume.find(params[:umamusume_id])
+    @race = Race.find(params[:race_id])
+
+    # レースの登録処理を実行
+    if @umamusume.won_race?(@race)
+      # すでに登録済みの場合の処理
+      flash[:alert] = "このレースはすでに登録されています。"
+    else
+      # レースを登録する処理
+      @umamusume.races << @race
+      flash[:notice] = "レースが登録されました。"
+    end
+
+    redirect_to umamusume_path(@umamusume)
   end
 
   def destroy
-    # レースの解除処理をここに実装
+    umamusume = Umamusume.find(params[:umamusume_id])
+    race = Race.find(params[:id])
+    record = RecordOfWin.find_by(umamusume: umamusume, race: race)
+
+    if record
+      record.destroy
+    end
+
+    redirect_to umamusume_path(umamusume)
   end
+
 end
