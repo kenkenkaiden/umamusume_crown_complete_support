be104ca553bc9ea13a801130f7908e51ca7926e2
kenkenkaiden <allexhard2000@gmail.com>
2023-11-06 14:21:48 +0900

    勝ち鞍が登録されているウマ娘を削除できる機能

--- app/models/umamusume.rb
+++ app/models/umamusume.rb
@@ -1,9 +1,9 @@
 class Umamusume < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :target_races
+  has_many :target_races, dependent: :destroy
   has_many :races, through: :target_races
-  has_many :record_of_wins
+  has_many :record_of_wins, dependent: :destroy
   has_many :races, through: :record_of_wins
 
   def won_race?(race) # ウマ娘が特定のレースで勝利したかどうかを確認するメソッド
