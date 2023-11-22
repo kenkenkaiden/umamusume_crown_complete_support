8b57b6c1fcec89d37d93ff527e5cfe7e7ff48372
kenkenkaiden <allexhard2000@gmail.com>
2023-11-22 22:59:06 +0900

    スペシャルウィークの目標レース投入

--- db/fixtures/03_TargetRace.rb
+++ db/fixtures/03_TargetRace.rb
@@ -12,32 +12,32 @@ TargetRace.seed(
     period: 'classic'
   },
   {
-    id: '18',
-    umamusume_name: 'トウカイテイオー',
+    id: '3',
+    umamusume_name: 'スペシャルウィーク',
     race_id: '10',
     period: 'classic'
   },
   {
-    id: '19',
-    umamusume_name: 'トウカイテイオー',
+    id: '4',
+    umamusume_name: 'スペシャルウィーク',
     race_id: '15',
     period: 'classic'
   },
   {
-    id: '20',
-    umamusume_name: 'トウカイテイオー',
+    id: '5',
+    umamusume_name: 'スペシャルウィーク',
     race_id: '6',
     period: 'senior'
   },
   {
-    id: '21',
-    umamusume_name: 'トウカイテイオー',
+    id: '6',
+    umamusume_name: 'スペシャルウィーク',
     race_id: '19',
     period: 'senior'
   },
   {
-    id: '22',
-    umamusume_name: 'トウカイテイオー',
+    id: '7',
+    umamusume_name: 'スペシャルウィーク',
     race_id: '23',
     period: 'senior'
   },
