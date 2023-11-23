c0f841cc35d13088ed9556f3b464f4d741a652da
kenkenkaiden <allexhard2000@gmail.com>
2023-11-23 22:55:52 +0900

    サイレンススズカの目標レース投入

--- db/fixtures/03_TargetRace.rb
+++ db/fixtures/03_TargetRace.rb
@@ -42,6 +42,49 @@ TargetRace.seed(
     period: 'senior'
   },
 
+  {
+    id: '8',
+    umamusume_name: 'サイレンススズカ',
+    race_id: '157',
+    period: 'junior'
+  },
+  {
+    id: '9',
+    umamusume_name: 'サイレンススズカ',
+    race_id: '42  ',
+    period: 'classic'
+  },
+  {
+    id: '10',
+    umamusume_name: 'サイレンススズカ',
+    race_id: '60',
+    period: 'classic'
+  },
+  {
+    id: '11',
+    umamusume_name: 'サイレンススズカ',
+    race_id: '43',
+    period: 'classic'
+  },
+  {
+    id: '12',
+    umamusume_name: 'サイレンススズカ',
+    race_id: '12',
+    period: 'senior'
+  },
+  {
+    id: '13',
+    umamusume_name: 'サイレンススズカ',
+    race_id: '62',
+    period: 'senior'
+  },
+  {
+    id: '14',
+    umamusume_name: 'サイレンススズカ',
+    race_id: '16',
+    period: 'senior'
+  },
+
 
   {
     id: '15',
