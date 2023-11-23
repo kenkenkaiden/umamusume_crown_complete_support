67c3ae2dd59af2f5af2e2a3c1b653f5b488cca65
kenkenkaiden <allexhard2000@gmail.com>
2023-11-24 01:58:36 +0900

    raceテーブルにサイレンススズカのメイクデビューを追加

--- db/fixtures/02_race.rb
+++ db/fixtures/02_race.rb
@@ -1,4 +1,28 @@
 Race.seed do |s|
+  s.id    = 156
+  s.name = 'メイクデビュー(スペシャルウィーク)'
+  s.grade = 'OP'
+  s.surface = '芝'
+  s.classification = '中距離'
+  s.distance = '2000'
+  s.date_junior = '6月'
+  s.course = '阪神'
+  s.default_order = 301
+
+Race.seed do |s|
+  s.id    = 157
+  s.name = 'メイクデビュー(サイレンススズカ)'
+  s.grade = 'OP'
+  s.surface = '芝'
+  s.classification = '中距離'
+  s.distance = '2000'
+  s.date_junior = '6月'
+  s.course = '阪神'
+  s.default_order = 302
+end
+
+
+Race.seed do |s|
   s.id    = 158
   s.name = 'メイクデビュー(トウカイテイオー)'
   s.grade = 'OP'
