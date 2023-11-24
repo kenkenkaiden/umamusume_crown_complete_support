3f8f0d2c09c621d7eb9cc3c15b769c7ae5b3316e
kenkenkaiden <allexhard2000@gmail.com>
2023-11-24 23:47:22 +0900

    Update 02_race.rb

--- db/fixtures/02_race.rb
+++ db/fixtures/02_race.rb
@@ -10,6 +10,7 @@ Race.seed do |s|
   s.default_order = 301
 end
 
+
 Race.seed do |s|
   s.id    = 157
   s.name = 'メイクデビュー(サイレンススズカ)'
@@ -25,6 +26,19 @@ end
 
 Race.seed do |s|
   s.id    = 158
+  s.name = 'メイクデビュー(トウカイテイオー)'
+  s.grade = 'OP'
+  s.surface = '芝'
+  s.classification = '中距離'
+  s.distance = '2000'
+  s.date_junior = '6月'
+  s.course = '中京'
+  s.default_order = 303
+end
+
+
+Race.seed do |s|
+  s.id    = 159
   s.name = 'メイクデビュー(トウカイテイオー)'
   s.grade = 'OP'
   s.surface = '芝'
