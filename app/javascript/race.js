096f70051dfa1f26e6087738d68cfdc526497caf
kenkenkaiden <allexhard2000@gmail.com>
2023-11-30 23:13:49 +0900

    レースボタンがきちんと取得できているかの確認

--- app/javascript/race.js
+++ app/javascript/race.js
@@ -4,9 +4,9 @@ window.addEventListener('load', function(){
   const maidenTargetRaceButtons = document.querySelectorAll(".btn.btn-outline-danger"); //未勝利の目標レースボタンを取得
   const wonTargetRaceButtons = document.querySelectorAll(".btn.btn-danger"); //勝利済みの目標レースボタンを取得
 
-  const buttonsArray = Array.from(raceButtons);
+  const wonTargetRaceButtonsArray = Array.from(wonTargetRaceButtons);
 
-  buttonsArray.forEach(function (button) {
+  wonTargetRaceButtons.forEach(function (button) {
     button.addEventListener('click', function (event) {
       event.preventDefault(); // デフォルトのボタンクリックを無効化
       console.log("Button Clicked");
