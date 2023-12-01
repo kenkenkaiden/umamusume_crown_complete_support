08acbca8c56dfe301896b1a173388b96e63feef7
kenkenkaiden <allexhard2000@gmail.com>
2023-12-01 23:17:49 +0900

    未勝利のレースボタンをクリックしたらJavaScriptで色を付ける
    ・DBにレース情報を送信はしていない
    ・未勝利のレースボタンをクリックすると、クリックしたレース以外のレースの勝利状況（色）が反転する

--- app/javascript/race.js
+++ app/javascript/race.js
@@ -4,12 +4,21 @@ window.addEventListener('load', function(){
   const maidenTargetRaceButtons = document.querySelectorAll(".btn.btn-outline-danger"); //未勝利の目標レースボタンを取得
   const wonTargetRaceButtons = document.querySelectorAll(".btn.btn-danger"); //勝利済みの目標レースボタンを取得
 
-  const wonTargetRaceButtonsArray = Array.from(wonTargetRaceButtons);
+// 未勝利のレースボタンのクリックイベント
+maidenRaceButtons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
 
-  wonTargetRaceButtons.forEach(function (button) {
-    button.addEventListener('click', function (event) {
-      event.preventDefault(); // デフォルトのボタンクリックを無効化
-      console.log("Button Clicked");
+    // 勝利済みのボタンと対応するデザインに切り替える処理
+    wonRaceButtons.forEach(function (wonButton) {
+      wonButton.classList.add("btn-dark");
+      wonButton.classList.remove("btn-primary");
     });
+
+    // 未勝利のボタンはそのまま
+    button.classList.remove("btn-dark");
+    button.classList.add("btn-primary");
   });
+});
+
 });
