85ab929568caf2d97398e4801b76fc35c2e2f960
kenkenkaiden <allexhard2000@gmail.com>
2023-12-02 23:18:24 +0900

    レースボタンをクリックしたときの挙動の調整

--- app/javascript/race.js
+++ app/javascript/race.js
@@ -4,20 +4,43 @@ window.addEventListener('load', function(){
   const maidenTargetRaceButtons = document.querySelectorAll(".btn.btn-outline-danger"); //未勝利の目標レースボタンを取得
   const wonTargetRaceButtons = document.querySelectorAll(".btn.btn-danger"); //勝利済みの目標レースボタンを取得
 
-// 未勝利のレースボタンのクリックイベント
+// 未勝利のレースボタンをクリックしたときの挙動
 maidenRaceButtons.forEach(function (button) {
   button.addEventListener('click', function (event) {
     event.preventDefault();
-
-    // 勝利済みのボタンと対応するデザインに切り替える処理
-    wonRaceButtons.forEach(function (wonButton) {
-      wonButton.classList.add("btn-dark");
-      wonButton.classList.remove("btn-primary");
-    });
-
-    // 未勝利のボタンはそのまま
     button.classList.remove("btn-dark");
     button.classList.add("btn-primary");
+  });
+});
+
+// 勝利済みのレースボタンをクリックしたときの挙動
+wonRaceButtons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+    button.classList.remove("btn-primary");
+    button.classList.add("btn-dark");
+  });
+});
+
+
+
+// 未勝利の目標レースボタンをクリックしたときの挙動
+maidenTargetRaceButtons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+    button.classList.remove("btn-outline-danger");
+    button.classList.add("btn-danger");
+  });
+});
+
+
+
+// 勝利済みの目標レースボタンをクリックしたときの挙動 これが上手く行かない
+wonTargetRaceButtons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+    button.classList.remove("btn-danger");
+    button.classList.add("btn-outline-danger");
   });
 });
 
