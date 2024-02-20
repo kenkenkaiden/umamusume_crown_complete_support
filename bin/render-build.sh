bc215733e6d9d1e69cd0ed009d935f4b22062ec0
kenkenkaiden <allexhard2000@gmail.com>
2024-02-21 04:59:27 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+bundle exec rails db:migrate
 bundle exec rake db:seed_fu RAILS_ENV=production
